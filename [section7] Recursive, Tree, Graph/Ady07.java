804baf5c5195c35796fdb2629d542057f50ec7fc
doyeahn <59862666+doyeahn@users.noreply.github.com>
2023-09-21 16:53:48 +0900

    [도예] Section 07 문풀

--- [section7] Recursive, Tree, Graph/Ady07.java
+++ [section7] Recursive, Tree, Graph/Ady07.java
@@ -1,32 +1,33 @@
-import java.util.Scanner;
-import java.util.Stack;
+class Node{
+    int data;
+    Node lt, rt; 
+    public Node(int val){
+        data=val;
+        lt=rt=null;
+    }
+}
 
-public class Main {
-	
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-
-		String s = in.next(); 
-		int answer = 0; 
-		
-		Stack<Character> stack = new Stack<>();
-		for(int i=0; i<s.length(); i++) {
-			if(s.charAt(i) == ')') {
-				if(s.charAt(i-1)== '(') { 
-					stack.pop();
-					answer += stack.size();
-				}
-				else { 
-					stack.pop();
-					answer += 1;
-				}
-			}
-			else {
-				stack.push('(');
-			}
-		}
-		
-		System.out.print(answer);
-
-	}
+public class Main{
+    Node root;
+    public void DFS(Node root){
+        if(root==null) return; 
+        else{
+            // System.out.print(root.data+" "); // 전위 순회
+            DFS(root.lt);
+            // System.out.print(root.data+" "); // 중위 순회
+            DFS(root.rt);
+            // System.out.print(root.data+" "); // 후위 순회
+        }
+    }
+    public static void main(String[] args) {
+        Main tree=new Main();
+        tree.root=new Node(1);
+        tree.root.lt=new Node(2);
+        tree.root.rt=new Node(3);
+        tree.root.lt.lt=new Node(4);
+        tree.root.lt.rt=new Node(5);
+        tree.root.rt.lt=new Node(6);
+        tree.root.rt.rt=new Node(7);
+        tree.DFS(tree.root);
+    }
 }
