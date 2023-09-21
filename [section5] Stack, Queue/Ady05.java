02b7fc7b02dcffaf600fbc991bcb5f045d48ce25
doyeahn <59862666+doyeahn@users.noreply.github.com>
2023-09-21 16:42:22 +0900

    [도예] Section 05 문풀

--- [section5] Stack, Queue/Ady05.java
+++ [section5] Stack, Queue/Ady05.java
@@ -1,44 +1,32 @@
+import java.util.Scanner;
+import java.util.Stack;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 
-public class Main3_3 {
+		String s = in.next(); 
+		int answer = 0; 
+		
+		Stack<Character> stack = new Stack<>();
+		for(int i=0; i<s.length(); i++) {
+			if(s.charAt(i) == ')') {
+				if(s.charAt(i-1)== '(') { 
+					stack.pop();
+					answer += stack.size();
+				}
+				else { 
+					stack.pop();
+					answer += 1;
+				}
+			}
+			else {
+				stack.push('(');
+			}
+		}
+		
+		System.out.print(answer);
 
-    public static int solution(int n, int k, int[] arr) {
-        int answer = 0;
-
-        for (int i = 0; i < n - k; i++) {
-            int sum = 0;
-
-            //i~(i+k) 날까지의 매출액
-            for (int j = i; j < i + k; j++) {
-                sum += arr[j];
-            }
-
-            if (sum > answer) {
-                answer = sum;
-            }
-        }
-
-        return answer;
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int k = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n];
-
-        st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; st.hasMoreTokens(); i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        System.out.println(solution(n, k, arr));
-    }
+	}
 }
