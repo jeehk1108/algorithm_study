0c1942c6441e48c32ecfdbaaa9cb62c26497d55f
doyeahn <59862666+doyeahn@users.noreply.github.com>
2023-09-21 16:54:38 +0900

    [도예] Section 08 문풀

--- [section8] DFS, BFS 활용/Ady08.java
+++ [section8] DFS, BFS 활용/Ady08.java
@@ -1,43 +1,41 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
+	static int maxScore = 0;
+	static int Num = 0;
+	static int Limit = 0;
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+	    Num = sc.nextInt(); 
+	    Limit = sc.nextInt();
+	    int sum = 0;
+	    
+	    int[][] question = new int[Num][2];
+	    
+	    for(int i = 0 ; i < Num ; i++) {
+	    	question[i][0] = sc.nextInt();
+	    	question[i][1] = sc.nextInt();
+	    }
+	    
+	    DFS(0, 0, 0, question);
+	    
+    	System.out.println(maxScore);
+	}
 
-    public static void solution(int n, int[] arr) {
-
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (arr[j] > arr[j + 1]) {
-                    swap(arr, j, j+1);
-                }
-            }
-        }
-    }
-
-    private static void swap(int[] arr, int n1, int n2) {
-        int temp = arr[n1];
-        arr[n1] = arr[n2];
-        arr[n2] = temp;
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
-        int[] arr = new int[n];
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; st.hasMoreTokens(); i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        solution(n, arr);
-
-        for (int i : arr) {
-            System.out.print(i + " ");
-        }
-
-    }
+	private static void DFS(int depth, int totalScore, int totalTime, int[][] question) {
+		if(totalTime > Limit) {
+			return;
+		}
+		
+		if(depth == Num) {
+			if(totalTime <= Limit && totalScore > maxScore) {
+				maxScore = totalScore;
+			}
+		} else {
+			DFS(depth+1, totalScore+question[depth][0], totalTime + question[depth][1], question);
+			DFS(depth+1, totalScore, totalTime, question);
+		}
+	}
 }
