fc0fd7e9a4afbb66ea1ab0ab28f24c33b02e8131
doyeahn <59862666+doyeahn@users.noreply.github.com>
2023-09-21 16:38:07 +0900

    [도예] Section 08 문풀

---
+++ [section8] DFS, BFS 활용/Ady08.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void solution(int n, int[] arr) {
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap(arr, j, j+1);
+                }
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int n1, int n2) {
+        int temp = arr[n1];
+        arr[n1] = arr[n2];
+        arr[n2] = temp;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; st.hasMoreTokens(); i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        solution(n, arr);
+
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
